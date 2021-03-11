62c56e035e7df11ac3eeca27d2ee2b4ef1430490
crushpowerx <asovin.e.v@gmail.com>
2021-03-11 15:57:06 +0200

    upd

---
+++ women/views.py
@@ -0,0 +1,30 @@
+from django.http import HttpResponse, HttpResponseNotFound
+from django.shortcuts import redirect, render
+
+from women.models import Women
+
+menu = ["О сайте", "Добавить статью", "Обратная связь", "Войти"]
+
+
+def index(request):
+    posts = Women.objects.all()
+    return render(request, 'women/index.html', {'posts': posts, 'menu': menu, 'title': 'Главная страница'})
+
+
+def about(request):
+    return render(request, 'women/about.html', {'menu': menu, 'title': 'О сайте'})
+
+
+def categories(request, catid):
+    return HttpResponse(f"<h1>Статьи по категориям</h1><p>{catid}<p>")
+
+
+def archive(request, year):
+    if int(year) > 2020:
+        return redirect('home')
+
+    return HttpResponse(f"<h1>Архив по годам</h1><p>{year}<p>")
+
+
+def pageNotFound(request, exception):
+    return HttpResponseNotFound("<h1>Страница не найдена</h1>")
