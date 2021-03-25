16c8173d8b2b43284d6600bbc7e0be5a359c38e5
crushpowerx <asovin.e.v@gmail.com>
2021-03-25 08:55:28 +0200

    upd

--- women/urls.py
+++ women/urls.py
@@ -5,7 +5,7 @@ from .views import *
 urlpatterns = [
     path('', index, name='home'),
     path('about/', about, name='about'),
-    path('addpage/', add_page, name='add_page'),
+    path('addpage/', addpage, name='add_page'),
     path('contact/', contact, name='contact'),
     path('login/', login, name='login'),
     path('post/<slug:post_slug>/', show_post, name='post'),
--- women/views.py
+++ women/views.py
@@ -1,15 +1,14 @@
 from django.http import HttpResponse, HttpResponseNotFound, Http404
-from django.shortcuts import render, get_object_or_404, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 
-from women.forms import AddPostForm
-from women.models import Women
+from .forms import *
+from .models import *
 
-menu = [
-    {'title': 'О сайте', 'url_name': 'about'},
-    {'title': 'Добавить статью', 'url_name': 'add_page'},
-    {'title': 'Обратная связь', 'url_name': 'contact'},
-    {'title': 'Войти', 'url_name': 'login'}
-]
+menu = [{'title': "О сайте", 'url_name': 'about'},
+        {'title': "Добавить статью", 'url_name': 'add_page'},
+        {'title': "Обратная связь", 'url_name': 'contact'},
+        {'title': "Войти", 'url_name': 'login'}
+        ]
 
 
 def index(request):
@@ -19,7 +18,7 @@ def index(request):
         'posts': posts,
         'menu': menu,
         'title': 'Главная страница',
-        'cat_selected': 0
+        'cat_selected': 0,
     }
 
     return render(request, 'women/index.html', context=context)
@@ -29,27 +28,28 @@ def about(request):
     return render(request, 'women/about.html', {'menu': menu, 'title': 'О сайте'})
 
 
-def add_page(request):
+def addpage(request):
     if request.method == 'POST':
-        form = AddPostForm(request.POST)
+        form = AddPostForm(request.POST, request.FILES)
         if form.is_valid():
             # print(form.cleaned_data)
-            try:
-                Women.objects.create(**form.cleaned_data)
-                return redirect('home')
-            except:
-                form.add_error(None, 'Ошибка добавления поста')
+            form.save()
+            return redirect('home')
     else:
         form = AddPostForm()
     return render(request, 'women/addpage.html', {'form': form, 'menu': menu, 'title': 'Добавление статьи'})
 
 
 def contact(request):
-    return HttpResponse('Обратная связь')
+    return HttpResponse("Обратная связь")
 
 
 def login(request):
-    return HttpResponse('Авторизация')
+    return HttpResponse("Авторизация")
+
+
+def pageNotFound(request, exception):
+    return HttpResponseNotFound('<h1>Страница не найдена</h1>')
 
 
 def show_post(request, post_slug):
@@ -59,7 +59,7 @@ def show_post(request, post_slug):
         'post': post,
         'menu': menu,
         'title': post.title,
-        'cat_selected': post.cat_id
+        'cat_selected': post.cat_id,
     }
 
     return render(request, 'women/post.html', context=context)
@@ -75,11 +75,7 @@ def show_category(request, cat_id):
         'posts': posts,
         'menu': menu,
         'title': 'Отображение по рубрикам',
-        'cat_selected': cat_id
+        'cat_selected': cat_id,
     }
 
     return render(request, 'women/index.html', context=context)
-
-
-def pageNotFound(request, exception):
-    return HttpResponseNotFound("<h1>Страница не найдена</h1>")
